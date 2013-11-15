5a97f68138c84f4a370093ca365287e080c255e3
Kevin Rutten <kevin11189@gmail.com>
2013-11-14 19:31:36 -0500

    Commit in Class. Test fight made.

--- GroupProject/src/GroupProject.java
+++ GroupProject/src/GroupProject.java
@@ -1,14 +1,15 @@
+import java.awt.*;
 
 public class GroupProject {
 
 	
 	public static void main(String[] args) {
-		
-		
+
 		Party party = new Party("Kevin", "Jessica", "Sadie");
 		
 		party.partyFight(new Monster("Demon"));
 
+        new Panel();
 	}
 
 }
